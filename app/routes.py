cc3e68419fc3b76da6eb749014c1a19642d1b0a8
Sumanth <sumugowd@gmail.com>
2026-03-29 23:56:18 +0530

    Added ticket creation API with role-based access

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint , request, jsonify, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from app.models.user import User
+from app.models.ticket import Ticket
 
 main = Blueprint('main', __name__)
 
@@ -72,3 +73,43 @@ def login():
 def logout():
     session.clear()
     return jsonify({"message": "Logged out"})
+
+# Create Ticket
+@main.route("/create-ticket", methods=["POST"])
+def create_ticket():
+    # Check login
+    if "user_id" not in session:
+        return jsonify({"error": "Login required"}), 401
+    
+    # Only staff allowed
+    if session.get("role") != "staff":
+        return jsonify({"error": "Only staff can create tickets"}), 403
+    
+    data = request.get_json()
+
+    title = data.get("title")
+    description = data.get("description")
+    issue_type = data.get("issue_type")
+    location = data.get("location")
+    phone = data.get("phone")
+
+    # Basic validation
+    if not all([title, description, issue_type, location, phone]):
+        return jsonify({"error": "All fields are required"}), 400
+    
+    ticket = Ticket(
+        title = title,
+        description = description,
+        issue_type = issue_type,
+        location = location,
+        created_by = session["user_id"],
+        phone = phone
+    )
+
+    db.session.add(ticket)
+    db.session.commit()
+
+    return jsonify({
+        "message": "Ticket created successfully",
+        "ticket_id" : ticket.id
+    })
